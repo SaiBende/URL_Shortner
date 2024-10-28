a594c8d443b21e9697da85a8d74020d67b4fc738
SaiBende <bendesai5703@gmail.com>
2024-10-28 23:29:47 +0530

    Refactor auth middleware to use authorization header instead of cookies

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,8 +1,9 @@
 import jwt from "jsonwebtoken";
 
 const auth = async (req, res, next) => {
-    console.log(req.cookies); // Log cookies for debugging
-    const token = req.cookies.token; // Get the token from cookies
+    // console.log(req.cookies); // Log cookies for debugging
+    // const token = req.cookies.token; // Get the token from cookies
+    const token=req.headers['authorization'];
 
     if (!token) {
         return res.status(401).json({ success: false, message: "You are not logged in" });
@@ -10,7 +11,7 @@ const auth = async (req, res, next) => {
 
     try {
         const decoded = jwt.verify(token, process.env.TOKEN_KEY);
-        console.log(decoded, "auth middleware cslltg");
+        console.log(decoded, "auth middleware called");
         req.user = decoded; // Store the decoded user info in the request object
         next(); // Proceed to the next middleware or route handler
     } catch (error) {
